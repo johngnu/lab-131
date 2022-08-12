ff987d91fe905d3ece01ee622d17cd58e088c56e
John Castillo <john.gnu@gmail.com>
2022-08-12 13:35:58 -0400

    r

--- tarea2/src/tarea2/Principal.java
+++ tarea2/src/tarea2/Principal.java
@@ -17,24 +17,29 @@ public class Principal {
     public static void main(String[] args) {
         Stack<String> A = new Stack<>();
         A.push("aaaa");
-        A.push("bbb");
+        A.push("bbba");
         A.push("ccc");
         A.push("axxx");
         A.push("ddd");
         A.push("zzz");
 
         System.out.println(A);
+        // a)
         contarCadenas(A);
         
-        Stack<String> C = new Stack<>();
+        // b)
+        System.out.println("llevando al final los que terminan con a");
+        llevarAlFinal(A);
+        
+        //Stack<String> C = new Stack<>();
         //vaciar(C, A);
-        System.out.println(C);
+        //System.out.println(C);
         System.out.println(A);
     }
 
     public static void contarCadenas(Stack<String> A) {
         Stack<String> aux = new Stack<>();
-        //vaciar(aux, A);
+
         int c=0;
         while (!A.empty()) {
             String s = A.pop();
@@ -45,6 +50,22 @@ public class Principal {
         }
         vaciar(A, aux);
         System.out.println("cadenas que empiezan con 'A' son: " + c);
+    }
+    
+    public static void llevarAlFinal(Stack<String> A) {
+        Stack<String> aux = new Stack<>();
+        Stack<String> as = new Stack<>();
+        int c=0;
+        while (!A.empty()) {
+            String s = A.pop();
+            if(s.charAt(s.length()-1) == 'a' || s.charAt(s.length()-1) == 'A') {
+                as.push(s);
+            } else {
+                aux.push(s);
+            }            
+        }
+        vaciar(A, aux);
+        vaciar(A, as);        
     }
 
 }
